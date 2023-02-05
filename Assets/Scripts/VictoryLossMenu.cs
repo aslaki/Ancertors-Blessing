8e8f8971312433d947247242c287029d133c2048
aslaki <aslaki@local>
2023-02-05 15:09:42 +0200

    victory text

--- Assets/Scripts/VictoryLossMenu.cs
+++ Assets/Scripts/VictoryLossMenu.cs
@@ -1,11 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class VictoryLossMenu : MonoBehaviour
 {
+    [SerializeField]
+    public GameObject victoryLossImage;
+
+    private void Start()
+    {
+        victoryLossImage.SetActive(false);
+        StartCoroutine(AppearText());
+    }
+
     public void OnClickMainMenu()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene("StartMenu");
     }
+
+    public IEnumerator AppearText()
+    {
+        yield return new WaitForSeconds(1.5f);
+        victoryLossImage.SetActive(true);
+        yield return new WaitForSeconds(5.0f);
+        UnityEngine.SceneManagement.SceneManager.LoadScene("StartMenu");
+    }
+    
+    
 }
